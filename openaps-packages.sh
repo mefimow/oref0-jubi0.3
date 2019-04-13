d56f678224083864d02f551ffd43bedd6912181e
sr.di <sr.di@ya.ru>
2019-04-13 16:29:18 +0300

    Patch rc.local

--- openaps-packages.sh
+++ openaps-packages.sh
@@ -6,6 +6,10 @@ sudo apt-get -o Acquire::ForceIPv4=true update && sudo apt-get -o Acquire::Force
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo systemctl disable bluetooth.service
+sudo echo "27c27
+< service bluetooth restart
+---
+> #service bluetooth restart" | patch /etc/rc.local
 sudo apt-get install libdbus-1-dev libglib2.0-dev libudev-dev libical-dev libreadline-dev -y && cd ~/src && wget www.kernel.org/pub/linux/bluetooth/bluez-5.50.tar.xz && tar xvf bluez-5.50.tar.xz && cd bluez-5.50 && ./configure --prefix=/usr --mandir=/usr/share/man --sysconfdir=/etc --localstatedir=/var --disable-systemd --enable-experimental && make -j4 && sudo make && sudo cp ./src/bluetoothd /usr/local/bin/ && \
 sudo apt-get -o Acquire::ForceIPv4=true install -y git python python-dev software-properties-common python-numpy python-pip watchdog strace tcpdump screen acpid vim locate jq lm-sensors && \
 #if getent passwd edison > /dev/null; then sudo apt-get -o Acquire::ForceIPv4=true install -y nodejs-legacy; fi && \
